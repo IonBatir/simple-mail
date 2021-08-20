ddb8f30cf3b8c96bacf45cbed86e1290bb9071e8
Ion Batîr <ionbatir@gmail.com>
2021-08-20 17:23:03 +0300

    Add storage mail functionality for mails service

--- src/app/services/mails.service.ts
+++ src/app/services/mails.service.ts
@@ -1,7 +1,7 @@
 import {Injectable} from '@angular/core';
 import {AngularFirestore, AngularFirestoreCollection} from '@angular/fire/firestore';
 import firebase from 'firebase';
-import FieldValue = firebase.firestore.FieldValue;
+import {Storage} from '@ionic/storage-angular';
 
 export type Mail = {
   id: string;
@@ -17,14 +17,28 @@ export type Mail = {
   providedIn: 'root'
 })
 export class MailsService {
+  private static mailKey = 'MAIL';
   private collection: AngularFirestoreCollection;
 
-  constructor(private firestore: AngularFirestore) {
+  constructor(private firestore: AngularFirestore, private storage: Storage) {
     this.collection = this.firestore.collection('mails');
+    this.storage.create().then(storageInstance => {
+      this.storage = storageInstance;
+    });
+  }
+
+  setMail(mail) {
+    this.storage.set(MailsService.mailKey, mail).then(() => {});
   }
 
   add(data) {
-    return this.collection.add({...data, from: '', date: FieldValue.serverTimestamp(), read: false});
+    return this.storage.get(MailsService.mailKey).then(mail =>
+      this.collection.add({
+        ...data,
+        from: mail,
+        date: firebase.firestore.FieldValue.serverTimestamp(),
+        read: false
+      }));
   }
 
   get(id) {
